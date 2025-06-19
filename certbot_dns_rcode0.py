8eb1b8eb4f4cd581553ef473c0778f18690acc78
Sascha Daniels <sd@alternative-solution.de>
2025-06-19 16:55:25 +0200

    Update certbot_dns_rcode0.py
    Added CNAME hanling for _acme-challenge

--- certbot_dns_rcode0.py
+++ certbot_dns_rcode0.py
@@ -1,90 +1,64 @@
-"""
-This module defines a certbot plugin to automate the process of completing a
-``dns-01`` challenge (`~acme.challenges.DNS01`) by creating, and subsequently
-removing, TXT records using the RcodeZero DNS API.
-"""
+import logging
+import dns.resolver
 
-# Keep metadata before any imports (for setup.py)!
-__version__ = '0.0.0.1'
-__url__     = 'https://github.com/nic-at/certbot-dns-rcode0'
-__all__     = ['Authenticator']
-
-from lexicon.providers import rcodezero
-import zope.interface
-
-from certbot import interfaces
 from certbot.plugins import dns_common
-from certbot.plugins import dns_common_lexicon
+from certbot.plugins.dns_common import CredentialsConfiguration
+from certbot.plugins.dns_common import DNSAuthenticator
 
-RCODE0_API_URL = 'https://my.rcodezero.at/'
+from rcode0_client import Rcode0Client
 
+logger = logging.getLogger(__name__)
 
-@zope.interface.implementer(interfaces.IAuthenticator)
-@zope.interface.provider(interfaces.IPluginFactory)
-class Authenticator(dns_common.DNSAuthenticator):
-    """DNS Authenticator for RcodeZero DNS
+class Authenticator(DNSAuthenticator):
+    """DNS Authenticator for RcodeZero DNS"""
 
-    This Authenticator uses the RcodeZero DNS API to fulfill a dns-01 challenge.
-    """
-
-    description = ('Obtain certificates using a DNS TXT record  for your'
-                   'domains hosted with RcodeZero DNS.')
+    description = 'Obtain certificates using a DNS TXT record (if you are using RcodeZero for DNS).'
 
     def __init__(self, *args, **kwargs):
-        super(Authenticator, self).__init__(*args, **kwargs)
-        self.credentials = None
-
-    @classmethod
-    def add_parser_arguments(cls, add):
-        super(Authenticator, cls).add_parser_arguments(add)
-        add('credentials', help='rcode0 credentials INI file.')
-
-    def more_info(self):
-        return ('This plugin configures a DNS TXT record to respond to a '
-                'dns-01 challenge using thei RcodeZero DNS API.')
+        super().__init__(*args, **kwargs)
+        self._rcode0_client = None
 
     def _setup_credentials(self):
-        self.credentials = self._configure_credentials(
+        self.credentials: CredentialsConfiguration = self._configure_credentials(
             'credentials',
-            'rcodezero credentials INI file',
+            'Path to RcodeZero DNS API credentials INI file',
             {
-                'api-key':      'ACME-token for RcodeZero DNS API,'
-                                'see https://my.rcodezero.at/api-doc'
+                'dns_rcode0_api_key': 'RcodeZero API key to access the DNS API',
             }
         )
 
     def _perform(self, domain, validation_name, validation):
-        self._get_rcode0_client().add_txt_record(
-            domain, validation_name, validation)
+        record_name = validation_name.rstrip('.')
+
+        # 🧠 CNAME-Auflösung
+        resolved = self._resolve_cname(record_name)
+        if resolved != record_name:
+            logger.info(f"CNAME erkannt für {record_name} → {resolved}")
+            record_name = resolved
+
+        self._get_rcode0_client().add_txt_record(record_name, validation)
 
     def _cleanup(self, domain, validation_name, validation):
-        self._get_rcode0_client().del_txt_record(
-            domain, validation_name, validation)
+        record_name = validation_name.rstrip('.')
+        resolved = self._resolve_cname(record_name)
+        if resolved != record_name:
+            logger.info(f"CNAME erkannt (cleanup) für {record_name} → {resolved}")
+            record_name = resolved
+
+        self._get_rcode0_client().del_txt_record(record_name, validation)
+
+    def _resolve_cname(self, name):
+        """Löst einen CNAME auf, gibt ansonsten den Originalnamen zurück"""
+        try:
+            answers = dns.resolver.resolve(name, 'CNAME')
+            return str(answers[0].target).rstrip('.')
+        except dns.resolver.NoAnswer:
+            return name
+        except Exception as e:
+            logger.warning(f"Fehler bei CNAME-Auflösung für {name}: {e}")
+            return name
 
     def _get_rcode0_client(self):
-        credentials = self.credentials.conf
-        return _Rcode0LexiconClient(
-            credentials('api-key'))
-    
-
-class _Rcode0LexiconClient(dns_common_lexicon.LexiconClient):
-    """Encapsulates all communication with the RcodeZeroDNS API via Lexicon."""
-
-    def __init__(self, api_key):
-        super(_Rcode0LexiconClient, self).__init__()
-        config = dns_common_lexicon.build_lexicon_config('rcode0', {}, {
-            'auth_token':       api_key
-        })
-        self.provider = rcodezero.Provider(config)
-
-    # called while guessing domain name (going from most specific to tld):
-    def _handle_http_error(self, e, domain_name):
-        if '404 Client Error:' in str(e):
-            return None
-        return super(_Rcode0LexiconClient, self)._handle_http_error(e, domain_name)
-
-    # called while guessing domain name (going from most specific to tld):
-    def _handle_general_error(self, e, domain_name):
-        if 'Value in field domainname does not match requirements' in str(e):
-            return None
-        return super(_Rcode0LexiconClient, self)._handle_general_error(e, domain_name)
+        if not self._rcode0_client:
+            self._rcode0_client = Rcode0Client(api_key=self.credentials.conf['dns_rcode0_api_key'])
+        return self._rcode0_client
